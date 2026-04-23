56ba0b67d510ff8971cf28295ee5526d5805f1ed
Paloma Reh <palomareh95@gmail.com>
2026-04-23 04:48:39 -0300

    feat: up and down

--- scripts/core/referee.gd
+++ scripts/core/referee.gd
@@ -4,6 +4,6 @@ class_name Referee
 var player: Player
 var opponent: Opponent
 
-func startMatch(player: Player, opponent: Opponent):
-	self.player = player
-	self.opponent = opponent
+func startMatch(p: Player, o: Opponent):
+	player = p
+	opponent = o
--- scripts/flow/game_scene.gd
+++ scripts/flow/game_scene.gd
@@ -4,6 +4,8 @@ var player
 var opponent
 var referee
 
+@onready var screens = $UI/Screens
+
 func _ready():
 	print("Game started")
 	setup_match()
@@ -19,3 +21,18 @@ func load_opponent_view():
 	var scene = load("res://scenes/opponents/opponent1.tscn")
 	var view = scene.instantiate()
 	$OpponentContainer.add_child(view)
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_down"):
+		show_config()
+
+	if Input.is_action_just_pressed("ui_up"):
+		show_match()
+
+func show_config():
+	var tween = create_tween()
+	tween.tween_property(screens, "position:y", -720, 0.3)
+
+func show_match():
+	var tween = create_tween()
+	tween.tween_property(screens, "position:y", 0, 0.3)
